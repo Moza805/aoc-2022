712d8dfc8beaccddd6bb727915e6182c81b9d516
Jack Thorpe <jack.thorpe@airelogic.com>
2022-12-08 12:44:18 +0000

    Reworked

--- Day 8/index.js
+++ Day 8/index.js
@@ -3,60 +3,66 @@ import fs from "fs";
 const forest = fs
   .readFileSync("./input.txt", "utf-8")
   .split(/\r?\n/)
-  .map((row) =>
-    row.split("").map((value) => ({
-      visible: false,
-      value,
-    }))
-  );
+  .map((row) => row.split("").map((x) => +x));
 
-const calculateVisibilityOnXAxis = (forest) => {
-  const updatedForest = [...forest];
-  let count = 0;
-  for (let rI = 1; rI < updatedForest[0].length - 1; rI++) {
-    for (let rC = 1; rC < updatedForest.length - 1; rC++) {
-      const tree = updatedForest[rI][rC];
-      if (tree.visible === false) {
-        const visibleLeft = updatedForest[rI]
-          .slice(0, rC)
-          .every((t) => t.value < tree.value);
+// const forest = [
+//   [3, 0, 3, 7, 3],
+//   [2, 5, 5, 1, 2],
+//   [6, 5, 3, 3, 2],
+//   [3, 3, 5, 4, 9],
+//   [3, 5, 3, 9, 0],
+// ];
 
-        const visible =
-          visibleLeft ||
-          updatedForest[rI].slice(rC + 1).every((t) => t.value < tree.value);
+const getVisibilityMatrix = (forest) => {
+  const matrix = [...forest.map((x) => x.slice())];
 
-        if (visible) {
-          count++;
-          updatedForest[rI][rC].visible = true;
-        }
-      }
+  for (let rI = 1; rI < forest[0].length - 1; rI++) {
+    for (let rC = 1; rC < forest.length - 1; rC++) {
+      const tree = forest[rI][rC];
+      const visibleLeft = forest[rI].slice(0, rC).every((t) => t < tree);
+
+      const visible = forest[rI].slice(rC + 1).every((t) => t < tree);
+
+      matrix[rI][rC] = visibleLeft || visible;
     }
   }
-  return [count, updatedForest];
+
+  return matrix;
 };
 
-const rotate = (forest, direction) => {
-  const rotatedForest = new Array(
-    forest[0].map((_) => new Array(forest.length))
+const rotate = (forest, direction = "right") => {
+  const matrix = [...forest.map((x) => x.slice())];
+
+  const rotatedMatrix = new Array(
+    matrix[0].map((_) => new Array(matrix.length))
   )[0];
 
-  if (direction === "right") {
-    forest.forEach((row, rowIdx) =>
-      row.forEach((col, colIdx) => {
-        rotatedForest[colIdx][rowIdx] = col;
-      })
-    );
+  direction === "left" && matrix.map((x) => x.reverse());
 
-    rotatedForest.forEach((x) => x.reverse());
-  }
+  matrix.forEach((row, rowIdx) =>
+    row.forEach((col, colIdx) => {
+      rotatedMatrix[colIdx][rowIdx] = col;
+    })
+  );
 
-  return rotatedForest;
+  direction === "right" && rotatedMatrix.map((x) => x.reverse());
+
+  return rotatedMatrix;
 };
 
 let tally = 2 * forest.length + 2 * forest[0].length - 4;
 
-const [xCount, updatedForest] = calculateVisibilityOnXAxis(forest);
-const rotatedForest = rotate(updatedForest, "right");
-const [yCount] = calculateVisibilityOnXAxis(rotatedForest);
+const xMatrix = getVisibilityMatrix(forest);
+const rotatedForest = rotate(forest, "right");
+const y = getVisibilityMatrix(rotatedForest);
+const yMatrix = rotate(y, "left");
 
-console.log(tally + xCount + yCount);
+var result = xMatrix.map((row, rowIdx) =>
+  row.map((col, colIdx) => {
+    return col || yMatrix[rowIdx][colIdx];
+  })
+);
+
+console.log(
+  tally + result.flatMap((row) => row.filter((y) => y === true)).length
+);
