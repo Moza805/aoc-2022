5f4c396663361a2c603e2a9c22cea299babf639a
Jack Thorpe <jack.thorpe@airelogic.com>
2022-12-09 16:20:10 +0000

    refactor

--- Day 9/index.js
+++ Day 9/index.js
@@ -5,10 +5,10 @@ const moves = fs
   .split(/\r?\n/)
   .map((row) => {
     const parts = row.split(" ");
-    const axis = ["U", "D"].includes(parts[0]) ? 1 : 0;
-    const distance = +parts[1] * (["U", "L"].includes(parts[0]) ? -1 : 1);
-
-    return { axis, distance };
+    return [
+      ["U", "D"].includes(parts[0]) ? 1 : 0,
+      +parts[1] * (["U", "L"].includes(parts[0]) ? -1 : 1),
+    ];
   });
 
 const createKnotArray = (knots) => {
@@ -23,30 +23,32 @@ const createKnotArray = (knots) => {
 
 const simulate = (moves, numberOfKnots) =>
   moves.reduce(
-    ({ knots, heatMap }, { axis, distance }) => {
+    ({ knots, heatMap }, [axis, distance]) => {
       for (let i = 0; i < Math.abs(distance); i++) {
-        for (const kIdx in knots) {
-          if (kIdx === "0") {
-            knots[0][axis] += distance < 0 ? -1 : 1;
-            continue;
+        knots = knots.reduce((result, knot) => {
+          if (result.length === 0) {
+            knot[axis] += distance < 0 ? -1 : 1;
+          } else {
+            const following = result[result.length - 1];
+            const [xDiff, yDiff] = [
+              following[0] - knot[0],
+              following[1] - knot[1],
+            ];
+            const [absX, absY] = [Math.abs(xDiff), Math.abs(yDiff)];
+
+            if (absX + absY > 2) {
+              knot[0] += xDiff > 0 ? 1 : -1;
+              knot[1] += yDiff > 0 ? 1 : -1;
+            } else if (absX > 1) {
+              knot[0] += xDiff > 0 ? 1 : -1;
+            } else if (absY > 1) {
+              knot[1] += yDiff > 0 ? 1 : -1;
+            }
           }
+          result.push(knot);
 
-          const [xDiff, yDiff] = [
-            knots[kIdx - 1][0] - knots[kIdx][0],
-            knots[kIdx - 1][1] - knots[kIdx][1],
-          ];
-
-          const [absXDiff, absYDiff] = [Math.abs(xDiff), Math.abs(yDiff)];
-
-          if (xDiff === 0 && absYDiff > 1) {
-            knots[kIdx][1] += yDiff > 0 ? 1 : -1;
-          } else if (yDiff === 0 && absXDiff > 1) {
-            knots[kIdx][0] += xDiff > 0 ? 1 : -1;
-          } else if (absXDiff > 1 || absYDiff > 1) {
-            knots[kIdx][0] += xDiff > 0 ? 1 : -1;
-            knots[kIdx][1] += yDiff > 0 ? 1 : -1;
-          }
-        }
+          return result;
+        }, []);
 
         const t = knots[numberOfKnots];
         heatMap[t[0]] = heatMap[t[0]] || {};
