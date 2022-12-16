cf057e534acdf6b6c7ff3caed14169e34d08347f
Jack Thorpe <jackthorpe1@hotmail.com>
2022-12-16 00:05:37 +0000

    started part b

--- Day 15/index.js
+++ Day 15/index.js
@@ -4,7 +4,7 @@ const getRectilinearDistance = (a, b) =>
   Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
 
 const data = fs
-  .readFileSync("./input.txt", "utf-8")
+  .readFileSync("./sample.txt", "utf-8")
   .split(/\r?\n/)
   .map((x) => x.match(/-?\d+/g))
   .map((x) => ({ loc: [+x[0], +x[1]], near: [+x[2], +x[3]] }))
@@ -13,24 +13,24 @@ const data = fs
     rad: getRectilinearDistance(x.loc, x.near),
   }));
 
-const yAxisData = data.reduce(
-  (result, sensor) => {
-    const options = [
-      result.min,
-      sensor.loc[0],
-      sensor.near[0],
-      result.max,
-    ].sort((a, b) => a - b);
-    const min = options.at(0) - sensor.rad;
-    const max = options.at(-1) + sensor.rad;
-    result.min = result.min > min ? min : result.min;
-    result.max = result.max < max ? max : result.max;
-    result.length = Math.abs(min) + Math.abs(max) + 1;
+// const yAxisData = data.reduce(
+//   (result, sensor) => {
+//     const options = [
+//       result.min,
+//       sensor.loc[0],
+//       sensor.near[0],
+//       result.max,
+//     ].sort((a, b) => a - b);
+//     const min = options.at(0) - sensor.rad;
+//     const max = options.at(-1) + sensor.rad;
+//     result.min = result.min > min ? min : result.min;
+//     result.max = result.max < max ? max : result.max;
+//     result.length = Math.abs(min) + Math.abs(max) + 1;
 
-    return result;
-  },
-  { min: 0, max: 0, length: 1 }
-);
+//     return result;
+//   },
+//   { min: 0, max: 0, length: 1 }
+// );
 
 // const row = 2000000; // 10 for sample.txt
 // const part1 = new Array(yAxisData.length).fill(null).reduce(
@@ -58,9 +58,28 @@ const yAxisData = data.reduce(
 
 // console.log("Part 1:", part1);
 
-const gridSize = 20;
-const searched = {};
-const partB = new Array(gridSize).fill(new Array(gridSize).fill(false));
+const gridSize = 21;
+const partB = new Array(gridSize)
+  .fill(null)
+  .map((_) => new Array(gridSize).fill(false));
 
-for (row of partB) {
+for (const sensor of data) {
+  for (
+    let x = sensor.loc[0] + sensor.rad * -1;
+    x <= sensor.loc[0] + sensor.rad;
+    x++
+  ) {
+    if (x >= 0 && x < gridSize) {
+      for (
+        let y = sensor.loc[1] + (Math.abs(x) - sensor.rad);
+        y <= sensor.loc[1] + (sensor.rad - Math.abs(x));
+        y++
+      ) {
+        if (y >= 0 && y < gridSize) {
+          partB[x][y] = true;
+        }
+      }
+    }
+  }
 }
+partB.map((x) => console.log(x.map((x) => (x ? "T" : "F")).join(" ")));
